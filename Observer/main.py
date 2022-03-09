6cc519c61e373182fa59714a096624dcb339a222
Vernitrax <ty21@op.pl>
2022-03-09 10:24:29 +0100

    if __name_ added

--- Observer/main.py
+++ Observer/main.py
@@ -3,13 +3,19 @@ from Observer.sensor_interpreter import WarpgateInterpreter
 from Observer.scanner import BioScanner, EnergyScanner
 from Observer.information_office import PropagandaOffice
 
-bio_scanner = BioScanner()
-energy_scanner = EnergyScanner()
-interpreter = WarpgateInterpreter(bio_scanner, energy_scanner)
-sensor = WarpgateSensor("r28-4c", interpreter)
-office = PropagandaOffice()
 
-interpreter.add_observer(office)
+def main():
+    bio_scanner = BioScanner()
+    energy_scanner = EnergyScanner()
+    interpreter = WarpgateInterpreter(bio_scanner, energy_scanner)
+    sensor = WarpgateSensor("r28-4c", interpreter)
+    office = PropagandaOffice()
 
-for i in range(10):
-    sensor.notify()
+    interpreter.add_observer(office)
+
+    for i in range(10):
+        sensor.notify()
+
+
+if __name__ == "__main__":
+    main()
